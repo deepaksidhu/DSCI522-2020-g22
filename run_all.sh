2c37bbbef85ce116f30fb6cc686b4a1635ce1db1
Gurdeepak Sidhu <gurdeepaksidhu@yahoo.com>
2020-11-26 14:18:39 -0800

    Added Script 3, EDA, and Bibliography

--- run_all.sh
+++ run_all.sh
@@ -11,11 +11,14 @@
 # download the data
 python src/downloadData.py --file_path=https://archive.ics.uci.edu/ml/machine-learning-databases/00529/diabetes_data_upload.csv --saving_path=data/raw_data.csv
 
+#run eda report
+Rscript -e "rmarkdown::render('src/diabetes_eda.Rmd',output_format='github_document')"
+
 # clean, pre-process data
 python src/clean_data.py --file_path=data/raw_data.csv --saving_path=data/cleaned_data.csv
 
 # create exploratory data analysis figure and write to file 
-Rscript src/eda_diab.r --preprocessed=data/raw_data.csv --out_dir=results/
+Rscript src/eda_diab.r --train=data/train_data.csv --out_dir=results/figures/
 
 # tune model
 
