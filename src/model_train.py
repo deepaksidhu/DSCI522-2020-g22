affcf0c82091491ab9cc83c0dd0fbd2f482dc5a9
kshahnazari1998 <kshahnazari@gmail.com>
2020-11-25 22:40:44 -0800

    Changed scoring to f1 score

--- src/model_train.py
+++ src/model_train.py
@@ -3,8 +3,9 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import recall_score, make_scorer, precision_score
+from sklearn.metrics import recall_score, make_scorer, precision_score, f1_score
 
 import pandas as pd
 import pickle
@@ -29,7 +30,9 @@ def train_report_save_model(
     pipe = make_pipeline(col_trans, model)
 
     def score_func(y_true, y_pred, **kwargs):
-        return recall_score(y_true, y_pred, pos_label="Positive")
+        return f1_score(y_true, y_pred, pos_label="Positive")
+        # Change to bellow if we want recall score
+        # return recall_score(y_true, y_pred, pos_label="Positive")
 
     scorer = make_scorer(score_func)
 
@@ -53,7 +56,9 @@ def train_report_save_model(
 
     # Return Recall of best metric on testing data
     y_test_predict = res.best_estimator_.predict(X_test)
-    return recall_score(y_test, y_test_predict, pos_label="Positive")
+    return f1_score(y_test, y_test_predict, pos_label="Positive")
+    # Change to below if we want recall score
+    # return recall_score(y_test, y_test_predict, pos_label="Positive")
 
 
 # Read data and split X and y
@@ -75,6 +80,8 @@ col_trans = ColumnTransformer(
 )
 
 # Hyperparameter tuning of the models and saving recall score of tests
+scores = []
+models = []
 
 # Decision Tree
 model = DecisionTreeClassifier()
@@ -84,15 +91,67 @@ decision_tree_pipe_hyperparamters = {
     "decisiontreeclassifier__min_samples_leaf": range(1, 5),
 }
 
-print(
-    train_report_save_model(
-        model,
-        decision_tree_pipe_hyperparamters,
-        X_train,
-        y_train,
-        X_test,
-        y_test,
-        save_dir_models,
-        save_dir_results,
-    )
+score = train_report_save_model(
+    model,
+    decision_tree_pipe_hyperparamters,
+    X_train,
+    y_train,
+    X_test,
+    y_test,
+    save_dir_models,
+    save_dir_results,
+)
+
+scores.append(score)
+models.append("Decision_Tree")
+# GaussianNB
+model = GaussianNB()
+
+gaussiannb_pipe_hyperparamters = {
+    "gaussiannb__var_smoothing": [10 ** pow for pow in range(-7, 5)]
+}
+
+score = train_report_save_model(
+    model,
+    gaussiannb_pipe_hyperparamters,
+    X_train,
+    y_train,
+    X_test,
+    y_test,
+    save_dir_models,
+    save_dir_results,
+)
+
+scores.append(score)
+models.append("GaussianNB")
+
+# Ridge
+
+model = LogisticRegression(random_state=123, max_iter=1000)
+
+logisticregression_pipe_hyperparamters = {
+    "logisticregression__C": [10 ** pow for pow in range(-7, 2)],
+    "logisticregression__solver": ["newton-cg", "lbfgs", "liblinear", "sag", "saga"],
+}
+
+score = train_report_save_model(
+    model,
+    logisticregression_pipe_hyperparamters,
+    X_train,
+    y_train,
+    X_test,
+    y_test,
+    save_dir_models,
+    save_dir_results,
+)
+
+scores.append(score)
+models.append("LogisticRegression")
+
+# Save Recall scores for models to a file
+
+pd.DataFrame({"model_name": models, "recall_score": scores}).to_csv(
+    save_dir_results + "_test_recall_scores.csv",
+    index_label=False,
+    index=False,
 )
